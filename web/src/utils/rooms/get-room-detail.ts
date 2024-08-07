64efbc6fa82cf447aa03f0612a47463e13dd75de
NITHIN3387 <nithinn9980@gmail.com>
2024-08-07 23:13:41 +0530

    Production and development URL's updated

--- web/src/utils/rooms/get-room-detail.ts
+++ web/src/utils/rooms/get-room-detail.ts
@@ -9,7 +9,11 @@ export const getRoomDetail = async (roomId: string) => {
 
   if (!token) redirect("/sign-in");
 
-  const res = await fetch(`http://localhost:3000/api/room/${roomId}`, {
+  const URL = process.env.NODE_ENV === "production"
+    ? `${process.env.PRODUCTION_URL}/api/room/${roomId}`
+    : `${process.env.DEVELOPMENT_URL}/api/room/${roomId}`
+
+  const res = await fetch(URL, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
